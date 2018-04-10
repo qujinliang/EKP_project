7d6dc372e174ec6c9dfa777df35c7705eac16678
qujinliang <qujin_liang@163.com>
2018-04-10 14:04:15 +0800

    "添加db_data.py 删除监控查验过的发票"

---
+++ db_data.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+import re
+
+client = MongoClient('40.125.167.185',27017)
+db = client.user
+db.authenticate("user","ysyc-mongo-user")
+db = client.user
+collection = db.fc_invoice_info
+
+fp_list2 = ['87120411','58929139','00271772','11695353','19768327','06001047','66718525','02062714','11610387',
+			'01875257','05331852','50657158','06998169','22052218','47982351','02188236','00021010','09251966',
+			'01208520','09030275','13826269','12299469','07708742','01316456','00324121','02226976','02748815',
+			'04312540']
+
+for i in fp_list2:
+	collection.remove({'fphm':i})
